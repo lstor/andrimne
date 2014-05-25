349e01a5791f2b51eac96d32a4850c739ecbd6e0
Lars Storjord <larsstor@ifi.uio.no>
2014-05-26 01:17:51 +0200

    Implement 'maven_build'.
    Close #1.

--- andrimne/steps/maven_build.py
+++ andrimne/steps/maven_build.py
@@ -1,5 +1,10 @@
+from andrimne.config import config_or_default
+from andrimne.common import run_shell_command
 import logging
 
 
 def run(cfg):
     logging.info('running maven build')
+
+    flags = config_or_default(cfg, 'maven_flags', '')
+    run_shell_command('mvn %s clean install' % flags)
