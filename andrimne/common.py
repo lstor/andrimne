07031baffa5588bfd4090b7af669ac5942771167
Lars Storjord <larsstor@ifi.uio.no>
2014-05-30 22:22:58 +0200

    Don't fail run_shell_command just because logging fails.

--- andrimne/common.py
+++ andrimne/common.py
@@ -4,4 +4,10 @@ import subprocess
 
 def run_shell_command(command, charset='utf-8'):
     output = subprocess.check_output(command, shell=True)
-    logging.info('output:\n%s' % output.decode(charset).strip())
+
+    # Swallow *all* errors from logging. We really don't want to interrupt flow just because logging fails.
+    # noinspection PyBroadException
+    try:
+        logging.info('output:\n%s' % output.decode(charset).strip())
+    except:
+        pass
