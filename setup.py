dff5c691e11e895af3d2671d77007ad79052c9b1
Lars Storjord <larsstor@ifi.uio.no>
2014-05-30 19:09:12 +0200

    Change packaging to setuptools.

--- setup.py
+++ setup.py
@@ -1,13 +1,45 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'DESCRIPTION.rst'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='andrimne',
-    version='0.1-dev',
-    packages=['andrimne', 'andrimne.tests'],
-    url='http://github.com/lstor/andrimne',
-    license='MIT Licence',
+    version='0.0.1-dev',
+
+    description='Customizable set of tools for automating code deployment.',
+    long_description=long_description,
+
     author='Lars Storjord',
     author_email='larsstor@ifi.uio.no',
-    description='Customizable set of tools for automating code deployment.',
-    long_description=open('README.txt').read()
+    url='https://github.com/lstor/andrimne',
+    license='MIT',
+
+    keywords='build deploy automate',
+    packages='andrimne',
+
+    install_requires=['PyYAML'],
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ],
+
+    data_files=[
+        ('andrimne.yaml', ['config/andrimne.yaml']),
+    ],
+
+    entry_points={
+        'console_scripts': [
+            'andrimne=andrimne:main',
+        ],
+    },
 )
