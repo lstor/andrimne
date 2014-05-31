034d2f6908e3e981c5637009321740e2ff2603d1
Lars Storjord <larsstor@ifi.uio.no>
2014-05-31 11:57:20 +0200

    Set permission on output files.
    Close #4.

--- andrimne/steps/set_permissions.py
+++ andrimne/steps/set_permissions.py
@@ -1,5 +1,15 @@
+from andrimne.common import run_shell_command
+from andrimne.config import config_or_default
 import logging
 
 
 def run(cfg):
     logging.info('setting permissions')
+
+    version = cfg['version']
+    prefix = cfg['module_prefix']
+    modules = config_or_default(cfg, 'code_modules', [])
+
+    for module in modules:
+        filename = '%s%s/target/%s%s-%s.war' % (prefix, module, prefix, module, version)
+        run_shell_command('chmod a+rw %s' % filename)
